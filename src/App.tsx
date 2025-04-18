e65f7f5a73f26589426349ccc0f12607de9ab084
Julian <julianbraso@gmail.com>
2025-04-18 16:38:56 -0300

    Minor changes, about to change to branch

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,48 @@ import SideDrawer from './components/drawer/SideDrawer'
 import { AddItem } from './components/drawer/AddItem'
 import { EditItem } from './components/drawer/EditItem'
 import { LoadingSpinner } from './components/LoadingSpinner'
+import { ItemList } from './components/ItemList'
+
+const mockItems = [
+  {
+    "name": "test",
+    "description": "shortDes",
+    "quantity": 1,
+    "purchased": false
+  },
+  {
+    "name": "test2",
+    "description": "shortDes",
+    "quantity": 2,
+    "purchased": false
+  },
+  {
+    "name": "test3",
+    "description": "shortDes",
+    "quantity": 3,
+    "purchased": false
+  },
+]
 
 function App() {
-  const [drawerOpen, setDrawerOpen] = useState(true);
-  const [isEditing, setIsEditing] = useState(true);
-  const [isLoading, setIsLoading] = useState(true);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  ///wip
+  const [items, setItems] = useState(mockItems)
+
+  function closeDrawer(){
+    setDrawerOpen(false);
+  }
+
   return (
     <Box className="mainContainer">
       <NavBar />
       <Box className="center bodyContainer">
-        {isLoading ? <LoadingSpinner /> : <EmptyListMsg clickCallback={() => setDrawerOpen(true)} />}
+        {isLoading ? <LoadingSpinner /> : items.length < 1 ? <EmptyListMsg clickCallback={() => closeDrawer()} /> : <ItemList items={items} />}
       </Box>
-      <SideDrawer isOpen={drawerOpen} onClose={() => setDrawerOpen(false)}>
-        {isEditing ? <EditItem cancelCallback={() => { }} /> : <AddItem cancelCallback={() => setDrawerOpen(false)} />}
+      <SideDrawer isOpen={drawerOpen} onClose={() => closeDrawer()}>
+        {isEditing ? <EditItem cancelCallback={() => closeDrawer()} /> : <AddItem cancelCallback={() => closeDrawer()} />}
       </SideDrawer>
     </Box>
   )
